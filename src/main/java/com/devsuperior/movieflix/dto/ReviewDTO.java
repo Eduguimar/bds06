6eba45eb9143dbc5f5fef3d267115609c1c9e8e3
Eduardo Guimaraes <eduguimara@gmail.com>
2021-08-11 19:41:37 -0300

    Created Movie, Genre and Review DTO

---
+++ src/main/java/com/devsuperior/movieflix/dto/ReviewDTO.java
@@ -0,0 +1,40 @@
+package com.devsuperior.movieflix.dto;
+
+import com.devsuperior.movieflix.entities.Review;
+
+import java.io.Serializable;
+
+public class ReviewDTO implements Serializable {
+
+    private Long id;
+    private String text;
+
+    public ReviewDTO() {
+    }
+
+    public ReviewDTO(Long id, String text) {
+        this.id = id;
+        this.text = text;
+    }
+
+    public ReviewDTO(Review review) {
+        this.id = review.getId();
+        this.text = review.getText();
+    }
+
+    public Long getId() {
+        return id;
+    }
+
+    public void setId(Long id) {
+        this.id = id;
+    }
+
+    public String getText() {
+        return text;
+    }
+
+    public void setText(String text) {
+        this.text = text;
+    }
+}

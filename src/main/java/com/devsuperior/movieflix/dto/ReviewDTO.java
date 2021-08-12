3b23da55a302e6a799ed8db1d8e619fa5b6df0e1
Eduardo Guimaraes <eduguimara@gmail.com>
2021-08-12 20:38:16 -0300

    Review POST endpoint

--- src/main/java/com/devsuperior/movieflix/dto/ReviewDTO.java
+++ src/main/java/com/devsuperior/movieflix/dto/ReviewDTO.java
@@ -1,28 +1,39 @@
 package com.devsuperior.movieflix.dto;
 
 import com.devsuperior.movieflix.entities.Review;
+import com.devsuperior.movieflix.entities.User;
 
+import javax.validation.constraints.NotBlank;
 import java.io.Serializable;
 
 public class ReviewDTO implements Serializable {
 
     private Long id;
 
-
+    @NotBlank(message = "Campo requerido")
     private String text;
     private Long movieId;
+    private UserDTO user;
 
     public ReviewDTO() {
     }
 
-    public ReviewDTO(Long id, String text) {
+    public ReviewDTO(Long id, String text, Long movieId, User user) {
         this.id = id;
         this.text = text;
+        this.movieId = movieId;
+        this.user = new UserDTO(user);
     }
 
     public ReviewDTO(Review review) {
         this.id = review.getId();
         this.text = review.getText();
+        this.movieId = review.getMovie().getId();
+    }
+
+    public ReviewDTO(Review review, User user) {
+        this(review);
+        this.user = new UserDTO(user);
     }
 
     public Long getId() {
@@ -47,5 +58,13 @@ public class ReviewDTO implements Serializable {
 
     public void setMovieId(Long movieId) {
         this.movieId = movieId;
+    }
+
+    public UserDTO getUser() {
+        return user;
+    }
+
+    public void setUser(UserDTO user) {
+        this.user = user;
     }
 }

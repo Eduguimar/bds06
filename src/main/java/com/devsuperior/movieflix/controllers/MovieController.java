bca384f004ee2e7069d5cd0aac91d0a9b9bd4316
Eduardo Guimaraes <eduguimara@gmail.com>
2021-08-12 21:14:50 -0300

    FindByMovie endpoint ok

--- src/main/java/com/devsuperior/movieflix/controllers/MovieController.java
+++ src/main/java/com/devsuperior/movieflix/controllers/MovieController.java
@@ -1,12 +1,16 @@
 package com.devsuperior.movieflix.controllers;
 
 import com.devsuperior.movieflix.dto.MovieDTO;
+import com.devsuperior.movieflix.dto.ReviewDTO;
 import com.devsuperior.movieflix.services.MovieService;
+import com.devsuperior.movieflix.services.ReviewService;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.data.domain.Page;
 import org.springframework.data.domain.Pageable;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.*;
+
+import java.util.List;
 
 @RestController
 @RequestMapping(value = "/movies")
@@ -14,6 +18,9 @@ public class MovieController {
 
     @Autowired
     private MovieService service;
+
+    @Autowired
+    private ReviewService reviewService;
 
     @GetMapping
     public ResponseEntity<Page<MovieDTO>> findAll(@RequestParam(name = "genreId", defaultValue = "0") Long genreId, Pageable pageable) {
@@ -25,5 +32,11 @@ public class MovieController {
     public ResponseEntity<MovieDTO> findById(@PathVariable Long id) {
         MovieDTO movie = service.findById(id);
         return ResponseEntity.ok().body(movie);
+    }
+
+    @GetMapping(value = "/{id}/reviews")
+    public ResponseEntity<List<ReviewDTO>> findReviewsByMovie(@PathVariable Long id) {
+        List<ReviewDTO> reviews = reviewService.findByMovie(id);
+        return ResponseEntity.ok().body(reviews);
     }
 }
--- src/main/java/com/devsuperior/movieflix/services/ReviewService.java
+++ src/main/java/com/devsuperior/movieflix/services/ReviewService.java
@@ -1,6 +1,7 @@
 package com.devsuperior.movieflix.services;
 
 import com.devsuperior.movieflix.dto.ReviewDTO;
+import com.devsuperior.movieflix.entities.Movie;
 import com.devsuperior.movieflix.entities.Review;
 import com.devsuperior.movieflix.entities.User;
 import com.devsuperior.movieflix.repositories.MovieRepository;
@@ -8,6 +9,9 @@ import com.devsuperior.movieflix.repositories.ReviewRepository;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
 import org.springframework.transaction.annotation.Transactional;
+
+import java.util.List;
+import java.util.stream.Collectors;
 
 @Service
 public class ReviewService {
@@ -20,6 +24,16 @@ public class ReviewService {
 
     @Autowired
     private AuthService authService;
+
+    public List<ReviewDTO> findByMovie(Long movieId) {
+        User user = authService.authenticated();
+        Movie movie = movieRepository.getOne(movieId);
+        List<Review> reviews = repository.findByMovie(movie);
+
+        return reviews.stream().map(review -> new ReviewDTO(review, user)).collect(Collectors.toList());
+    }
+
+
 
     @Transactional
     public ReviewDTO insert(ReviewDTO dto) {
